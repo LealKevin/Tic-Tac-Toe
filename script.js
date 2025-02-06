d689cd81589addf95fb9c9c556a9d9502f4b69ca
Mizuba74 <kevinleal@hotmail.com>
2025-02-06 20:41:50 +0100

    feature: name change, and button reset

--- script.js
+++ script.js
@@ -4,12 +4,94 @@ const game = {
     player1: 'X',
     player2: 'O',
     currentPlayer: 'X',
+    playerXName: 'Player X',
+    playerOName: 'Player O',
+    playerXScore: 0,
+    playerOScore: 0,
+    message: `Welcome! You can change player's name by double click on it!`,
+    get score() {
+        return `${this.playerXScore} : ${this.playerOScore}`;
+    },
     render() {
         const root = document.querySelector('#root');
         if (root === null) {
             throw new Error;
         }
         root.classList.add('root');
+        if (!(document.querySelector('.players'))) {
+            const players = document.createElement('div');
+            const playerX = document.createElement('div');
+            const playerO = document.createElement('div');
+            const scores = document.createElement('div');
+            const message = document.createElement('div');
+            message.classList.add('message');
+            playerX.classList.add('playerX');
+            playerO.classList.add('player');
+            message.textContent = '';
+            message.textContent = this.message;
+            scores.textContent = this.score;
+            playerX.textContent = this.playerXName;
+            playerO.textContent = this.playerOName;
+            playerX.addEventListener('dblclick', () => {
+                const textInput = document.createElement('input');
+                const ok = document.createElement('button');
+                textInput.classList.add('nameChange');
+                ok.textContent = 'Ok!';
+                ok.classList.add('okBtn');
+                ok.type = 'submit';
+                ok.addEventListener('click', () => {
+                    const newName = textInput.value;
+                    playerX.textContent = newName;
+                    this.playerXName = newName;
+                });
+                textInput.inputMode = 'text';
+                playerX.textContent = '';
+                playerX.appendChild(textInput);
+                playerX.appendChild(ok);
+            });
+            playerO.addEventListener('dblclick', () => {
+                const textInput = document.createElement('input');
+                const ok = document.createElement('button');
+                ok.textContent = 'Ok!';
+                ok.classList.add('okBtn');
+                ok.type = 'submit';
+                textInput.classList.add('nameChange');
+                textInput.inputMode = 'text';
+                ok.addEventListener('click', () => {
+                    const newName = textInput.value;
+                    playerO.textContent = newName;
+                    this.playerOName = newName;
+                });
+                playerO.textContent = '';
+                playerO.appendChild(textInput);
+                playerO.appendChild(ok);
+            });
+            players.classList.add('players');
+            scores.classList.add('score');
+            players.appendChild(playerX);
+            players.appendChild(scores);
+            players.appendChild(playerO);
+            root.appendChild(message);
+            root.appendChild(players);
+        }
+        if (!(document.querySelector('.reset'))) {
+            const reset = document.createElement('button');
+            reset.classList.add('reset');
+            reset.textContent = 'Reset scores';
+            reset.addEventListener('click', () => {
+                this.playerXScore = 0;
+                this.playerOScore = 0;
+                const score = document.querySelector('.score');
+                if (score) {
+                    score.textContent = this.score;
+                }
+                const message = document.querySelector('.message');
+                if (message) {
+                    message.textContent = 'Good luck!';
+                }
+            });
+            root.appendChild(reset);
+        }
         if (!(document.querySelector('.grid'))) {
             const grid = document.createElement('div');
             grid.classList.add('grid');
@@ -59,14 +141,15 @@ const game = {
             return 'O';
         }
         //Rows check
-        this.board.forEach((row) => {
+        for (let row of this.board) {
             if (row.every((cell) => cell === "X")) {
                 return 'X';
             }
             if (row.every((cell) => cell === "O")) {
                 return 'O';
             }
-        });
+        }
+        ;
         //Columns check
         for (let i = 0; i < this.board.length; i++) {
             const column = [this.board[0][i], this.board[1][i], this.board[2][i]];
@@ -101,32 +184,46 @@ const game = {
     finishGame(winner) {
         this.currentPlayer = 'X';
         if (winner === 'Tie') {
-            console.log(`It's a tie!`);
+            this.message = `It's a tie!`;
         }
         else {
-            console.log("The winner is: " + winner);
+            if (winner === 'X') {
+                this.message = "The winner is: " + this.playerXName + '!';
+            }
+            else {
+                this.message = "The winner is: " + this.playerOName + '!';
+            }
+        }
+        if (winner === 'X') {
+            this.playerXScore += 1;
+        }
+        if (winner === 'O') {
+            this.playerOScore += 1;
         }
         const grid = document.querySelector('.grid');
         if (grid) {
             grid.remove();
+        }
+        const message = document.querySelector('.message');
+        if (message) {
+            message.textContent = this.message;
+        }
+        const score = document.querySelector('.score');
+        if (score) {
+            score.textContent = this.score;
         }
         this.board = this.board.map(row => row.map(() => ''));
         this.render();
     },
     handlePositionClick(x, y) {
         this.playAt(x, y);
-        this.render();
         const winner = this.tryGetWinner();
         if (winner) {
             this.finishGame(winner);
         }
+        else {
+            this.render();
+        }
     }
 };
-class Player {
-    constructor(obj) {
-        this.type = obj.type;
-    }
-}
-const playerX = new Player({ type: 'X' });
-const playerO = new Player({ type: 'O' });
 game.render();
--- script.ts
+++ script.ts
@@ -1,149 +1,242 @@
+"use strict";
 const game = {
-    board: [['','',''], ['','',''],['','','']],
+    board: [['', '', ''], ['', '', ''], ['', '', '']],
     player1: 'X',
     player2: 'O',
     currentPlayer: 'X',
+    playerXName: 'Player X',
+    playerOName: 'Player O',
+    playerXScore: 0,
+    playerOScore: 0,
+    message: `Welcome! You can change player's name by double click on it!`,
+    get score() {
+        return `${this.playerXScore} : ${this.playerOScore}`;
+    },
+    render() {
+        const root = document.querySelector('#root');
 
-    render(){
-
-        const root:HTMLDivElement | null = document.querySelector('#root');
-        if(root === null){
+        if (root === null) {
             throw new Error;
         }
         root.classList.add('root');
 
-        if(!(document.querySelector('.grid'))){
-            const grid:HTMLDivElement = document.createElement('div');
-            grid.classList.add('grid');
 
-            for( let j = 0 ; j < 3 ; j++ ){
-                for( let i = 0 ; i < 3 ; i++ ){
-                    const cell:HTMLDivElement = document.createElement('div');
-                    if( j === 1 && i === 1){
-                        cell.classList.add('cellCen')
-                    } else if( i === 1 ){
-                        cell.classList.add('cellVert');
-                    }else if( j === 1 ){
-                        cell.classList.add('cellHor');
-                    }else{
-                        cell.classList.add('cell');
-                    }
-                    cell.dataset.coordinates = `${j},${i}`
-                    grid.appendChild(cell);
+        if (!(document.querySelector('.players'))) {
+            const players = document.createElement('div');
+            const playerX = document.createElement('div');
+            const playerO = document.createElement('div');
+            const scores = document.createElement('div');
+            const message = document.createElement('div');
 
-                    cell.addEventListener('click',() => {
-                            cell.textContent = this.currentPlayer;
-                            this.handlePositionClick(j, i) 
-                        })
-                        root.appendChild(grid);
-                }
-            }
+            message.classList.add('message');
+            playerX.classList.add('playerX');
+            playerO.classList.add('player');
+
+            message.textContent = '';
+            message.textContent = this.message;
+            scores.textContent = this.score;
+            playerX.textContent = this.playerXName;
+            playerO.textContent = this.playerOName;
+
+            playerX.addEventListener('dblclick', () => {
+                const textInput = document.createElement('input');
+                const ok = document.createElement('button');
+                textInput.classList.add('nameChange')
+                ok.textContent = 'Ok!';
+                ok.classList.add('okBtn');
+                ok.type = 'submit';
+                ok.addEventListener('click', () => {
+                    const newName = textInput.value;
+                    playerX.textContent = newName;
+                    this.playerXName = newName;
+                });
+                textInput.inputMode = 'text';
+                playerX.textContent = '';
+                playerX.appendChild(textInput);
+                playerX.appendChild(ok);
+            });
+            playerO.addEventListener('dblclick', () => {
+                const textInput = document.createElement('input');
+                const ok = document.createElement('button');
+                ok.textContent = 'Ok!';
+                ok.classList.add('okBtn');
+                ok.type = 'submit';
+                textInput.classList.add('nameChange')
+                textInput.inputMode = 'text';
+                ok.addEventListener('click', () => {
+                    const newName = textInput.value;
+                    playerO.textContent = newName;
+                    this.playerOName = newName;
+                });
+                playerO.textContent = '';
+                playerO.appendChild(textInput);
+                playerO.appendChild(ok);
+            });
+            players.classList.add('players');
+            scores.classList.add('score');
+            players.appendChild(playerX);
+            players.appendChild(scores);
+            players.appendChild(playerO);
+            root.appendChild(message);
+            root.appendChild(players);
         }
 
+        if(!( document.querySelector('.reset') )){
+            const reset = document.createElement('button'); 
+            reset.classList.add('reset');
+            reset.textContent = 'Reset scores';
 
-        this.board.forEach((row) =>{
+            reset.addEventListener('click', () => {
+                this.playerXScore = 0;
+                this.playerOScore = 0;
+
+                const score: HTMLDivElement | null = document.querySelector('.score')
+                if(score){
+                    score.textContent = this.score;
+                }
+
+                const message: HTMLDivElement | null = document.querySelector('.message');
+                if(message){
+                    message.textContent = 'Good luck!'
+                }
+            })
+
+            root.appendChild(reset);
+        }
+        if (!(document.querySelector('.grid'))) {
+            const grid = document.createElement('div');
+            grid.classList.add('grid');
+            for (let j = 0; j < 3; j++) {
+                for (let i = 0; i < 3; i++) {
+                    const cell = document.createElement('div');
+                    if (j === 1 && i === 1) {
+                        cell.classList.add('cellCen');
+                    }
+                    else if (i === 1) {
+                        cell.classList.add('cellVert');
+                    }
+                    else if (j === 1) {
+                        cell.classList.add('cellHor');
+                    }
+                    else {
+                        cell.classList.add('cell');
+                    }
+                    cell.dataset.coordinates = `${j},${i}`;
+                    grid.appendChild(cell);
+                    cell.addEventListener('click', () => {
+                        cell.textContent = this.currentPlayer;
+                        this.handlePositionClick(j, i);
+                    });
+                    root.appendChild(grid);
+                }
+            }
+
+
+
+
+        }
+        this.board.forEach((row) => {
             console.log(row.join(' | '));
         });
     },
-    tryGetWinner(): 'X' | 'O' | 'Tie' | null{
+    tryGetWinner() {
         const diagonal1 = [this.board[0][0], this.board[1][1], this.board[2][2]];
         const diagonal2 = [this.board[0][2], this.board[1][1], this.board[2][0]];
-
-//Diagonal check
-        if(diagonal1.every(cell => cell === "X")){
-            return 'X'
+        //Diagonal check
+        if (diagonal1.every(cell => cell === "X")) {
+            return 'X';
         }
-        if(diagonal1.every(cell => cell === "O")){
-            return 'O'
+        if (diagonal1.every(cell => cell === "O")) {
+            return 'O';
         }
-        if(diagonal2.every(cell => cell === "X")){
-            return 'X'
+        if (diagonal2.every(cell => cell === "X")) {
+            return 'X';
         }
-        if(diagonal2.every(cell => cell === "O")){
-            return 'O'
+        if (diagonal2.every(cell => cell === "O")) {
+            return 'O';
         }
-//Rows check
-        this.board.forEach((row) => {
-            if(row.every((cell) => cell === "X")){
-                return 'X'
+        //Rows check
+        for(let row of this.board){
+            if (row.every((cell) => cell === "X")) {
+                return 'X';
             }
-            if(row.every((cell) => cell === "O")){
-                return 'O'
+            if (row.every((cell) => cell === "O")) {
+                return 'O';
             }
-        });
-
-//Columns check
-       for(let i = 0 ; i < this.board.length ; i++){
-         const column = [this.board[0][i], this.board[1][i], this.board[2][i] ]        
-         if(column.every((cell) => cell === 'X')){
-            return 'X'  
-         }
-         if(column.every((cell) => cell === 'O')){
-            return 'O'
-         }
-       }
-
-//Tiec heck
-        const isBoardEmpty = this.board.every( row => row.every(cell => cell !== '') ) 
-        if(isBoardEmpty){
+        };
+        //Columns check
+        for (let i = 0; i < this.board.length; i++) {
+            const column = [this.board[0][i], this.board[1][i], this.board[2][i]];
+            if (column.every((cell) => cell === 'X')) {
+                return 'X';
+            }
+            if (column.every((cell) => cell === 'O')) {
+                return 'O';
+            }
+        }
+        //Tiec heck
+        const isBoardEmpty = this.board.every(row => row.every(cell => cell !== ''));
+        if (isBoardEmpty) {
             return 'Tie';
         }
     },
-
-    playAt(x: number,y: number){
-        if(!(this.board[x][y] === '')){
+    playAt(x:number, y:number) {
+        if (!(this.board[x][y] === '')) {
             alert(`Can't play on already played case`);
-        }else{
-            this.board[x][y] = this.currentPlayer ;
+        }
+        else {
+            this.board[x][y] = this.currentPlayer;
             //Change current player
-            if(this.currentPlayer === 'X'){
-                this.currentPlayer = 'O'
-            } else {
-                this.currentPlayer = 'X'
+            if (this.currentPlayer === 'X') {
+                this.currentPlayer = 'O';
+            }
+            else {
+                this.currentPlayer = 'X';
             }
         }
-
     },
-    finishGame(winner: string){
-        this.currentPlayer = 'X'
-        if(winner === 'Tie'){
-           console.log(`It's a tie!`);
-        }else{
-            console.log("The winner is: " + winner);
+    finishGame(winner: string) {
+        this.currentPlayer = 'X';
+        if (winner === 'Tie') {
+            this.message = `It's a tie!`
+        }
+        else {
+            if(winner === 'X'){
+                this.message = "The winner is: " + this.playerXName + '!';
+            } else {
+                this.message = "The winner is: " + this.playerOName + '!';
+            }
+        }
+        if (winner === 'X') {
+            this.playerXScore += 1;
+        }
+        if (winner === 'O') {
+            this.playerOScore += 1;
         }
         const grid = document.querySelector('.grid');
-        if(grid){
+        if (grid) {
             grid.remove();
         }
+        const message: HTMLDivElement | null = document.querySelector('.message');
+        if(message){
+            message.textContent = this.message;
+        }
+
+        const score = document.querySelector('.score')
+        if(score){
+            score.textContent = this.score;
+        }
         this.board = this.board.map(row => row.map(() => ''));
-        this.render()
-    }, 
-
-    handlePositionClick(x: number, y: number){
-        this.playAt(x,y);
         this.render();
-
-        const winner: string | null = this.tryGetWinner();
-        if(winner){
+    },
+    handlePositionClick(x: number, y:number) {
+        this.playAt(x, y);
+        const winner = this.tryGetWinner();
+        if (winner) {
             this.finishGame(winner);
+        }else {
+            this.render();
         }
     }
-
 };
-
-class Player{
-    type: string;
-
-    constructor(obj:{ type: string }){
-        this.type = obj.type;
-    }
-}
-
-const playerX = new Player({type: 'X'});
-
-
-const playerO = new Player({type: 'O'});
 game.render();
-
-
-
